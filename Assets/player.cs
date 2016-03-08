2d3f4f0e6f487314e84d8bcb3ff4ba0dc31b6b49
MitchLewis <mitchell.lewis@colorado.edu>
2016-03-07 21:32:48 -0700

    VR Pong Unity Files
    Unity files for virtual reality pong game using Google Cardboard

---
+++ Assets/player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public float speed = 20f;
+    
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inputSpeedVertical = Input.GetAxisRaw ("player");
+        float inputSpeedHorizontal = Input.GetAxisRaw ("playerHorizontal");
+
+
+
+        transform.position += (new Vector3 (inputSpeedHorizontal *speed * Time.deltaTime , inputSpeedVertical * speed * Time.deltaTime, 0f));
+
+        
+        
+
+
+    }
+}
