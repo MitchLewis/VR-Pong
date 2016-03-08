ad04875ebd85f4a7fe65f615be09ae4a1d26f1f5
MitchLewis <mitchell.lewis@colorado.edu>
2016-03-07 21:42:10 -0700

    Unity Assets
    Asset files from VR Pong Unity game

---
+++ Assets/ball1.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball1 : MonoBehaviour {
+
+	public Vector3 initialImpulse;
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<Rigidbody> ().AddForce (initialImpulse, ForceMode.Impulse);
+	
+	}
+
+    public static int playerScore = 0;
+    public static int opponentScore = 0;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.z > 19f)
+        {
+            playerScore++;
+            transform.position = new Vector3(-2.83f, 2.5f, -7.62f);
+
+        }
+
+        if (transform.position.z < -19f)
+        {
+            opponentScore++;
+            transform.position = new Vector3(-2.83f, 2.57f, -7.62f);
+        }
+	
+	}
+}
---
+++ Assets/score.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score : MonoBehaviour {
+
+   //public GameObject head;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        GetComponent<TextMesh>().text = string.Format("Player: {0} \nOpponet: {1}", ball1.playerScore, ball1.opponentScore);
+
+        //transform.parent = head.transform;
+    }
+}
